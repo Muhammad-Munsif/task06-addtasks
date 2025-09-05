4c7b9195e66147ef8d20f4eb1a2cb4730f4c6454
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-09-05 22:55:14 +0500

    small tasked added to JS file

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
-
 // Load tasks on page load
 document.addEventListener("DOMContentLoaded", loadTasksFromStorage);
-
 // Add Task
 addTaskBtn.addEventListener("click", () => {
   const taskText = taskInput.value.trim();
@@ -12,18 +10,15 @@ addTaskBtn.addEventListener("click", () => {
     alert("Please enter a task");
     return;
   }
-
   addTaskToList(taskText, false);
   saveTask(taskText, false);
   taskInput.value = "";
 });
-
-// Event Delegation: Delete or Mark as Done
+// Event Delegation: Delete or Mark as Done as tasks are dynamic
 taskList.addEventListener("click", function (e) {
   const target = e.target;
   const li = target.closest("li");
   const taskText = li.firstChild.textContent.trim();
-
   if (target.classList.contains("delete-btn")) {
     li.remove();
     removeTaskFromStorage(taskText);
@@ -33,36 +28,30 @@ taskList.addEventListener("click", function (e) {
     toggleTaskDoneInStorage(taskText);
   }
 });
-
 // Helpers
 function addTaskToList(taskText, done) {
   const li = document.createElement("li");
   if (done) li.classList.add("done");
-
   li.innerHTML = `
       ${taskText}
       <button class="delete-btn">Delete</button>
     `;
   taskList.appendChild(li);
 }
-
 function saveTask(taskText, done) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.push({ text: taskText, done });
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
 function loadTasksFromStorage() {
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.forEach((task) => addTaskToList(task.text, task.done));
 }
-
 function removeTaskFromStorage(taskText) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks = tasks.filter((task) => task.text !== taskText);
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
 function toggleTaskDoneInStorage(taskText) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks = tasks.map((task) =>
